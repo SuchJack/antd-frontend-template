527df580e902bb8dd0afcd7dc4dd98c014d39e74
SuchJack <2640808535@qq.com>
2024-12-30 15:21:55 +0800

    update

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,11 @@
-import { PageContainer } from '@ant-design/pro-components';
-import React, { useEffect, useState } from 'react';
+import {ActionType, PageContainer} from '@ant-design/pro-components';
+import React, {useEffect, useRef, useState} from 'react';
 import { Avatar, Card, List, message, Space, Tag, Typography } from 'antd';
 import { LikeOutlined, MessageOutlined, StarOutlined, UserOutlined } from '@ant-design/icons';
 import { listPostVoByPageUsingPost } from '@/services/backend/postController';
 import moment from 'moment';
+import {doThumbUsingPost} from "@/services/backend/postThumbController";
+import {doPostFavourUsingPost} from "@/services/backend/postFavourController";
 
 const { Text, Paragraph } = Typography;
 
@@ -21,6 +23,7 @@ const PostPage: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [dataList, setDataList] = useState<API.PostVO[]>([]);
   const [total, setTotal] = useState<number>(0);
+  const actionRef = useRef<ActionType>();
   const [searchParams, setSearchParams] = useState<API.PostQueryRequest>({
     ...DEFAULT_PAGE_PARAMS,
   });
@@ -41,8 +44,42 @@ const PostPage: React.FC = () => {
     loadData();
   }, [searchParams]);
 
-  const IconText = ({ icon, text, onClick }: { icon: React.FC; text: string; onClick?: () => void }) => (
-    <Space onClick={onClick} style={{ cursor: onClick ? 'pointer' : 'default' }}>
+
+  const handleThumb =  async (postId: number) => {
+    if (postId) {
+      try {
+        const res = await doThumbUsingPost({postId})
+        message.success(res.data === 1 ? '点赞成功' : '取消点赞成功');
+      } catch (e: any) {
+        message.error('点赞失败,' + e.message);
+      }
+    }
+  };
+  // 处理收藏
+  const handleFavour = async (postId: number) => {
+    if (postId) {
+      try {
+        const res = await doPostFavourUsingPost({postId})
+        message.success(res.data === 1 ? '收藏成功' : '取消收藏成功');
+      } catch (e: any) {
+        message.error('点赞失败,' + e.message);
+      }
+    }
+  };
+
+  const IconText = ({ icon, text, onClick, highlighted }: {
+    icon: React.FC;
+    text: string;
+    onClick?: () => void;
+    highlighted?: boolean;
+  }) => (
+    <Space
+      onClick={onClick}
+      style={{
+        cursor: onClick ? 'pointer' : 'default',
+        color: highlighted ? '#1890ff' : 'inherit',
+      }}
+    >
       {React.createElement(icon)}
       {text}
     </Space>
@@ -75,9 +112,9 @@ const PostPage: React.FC = () => {
             <List.Item
               key={item.id}
               actions={[
-                <IconText key={item.id} icon={LikeOutlined} text={String(item.favourNum ?? 0)} />,
+                <IconText key={item.id} icon={LikeOutlined} text={String(item.thumbNum ?? 0)}  onClick={() => handleThumb((item.id) as number)} highlighted={item.hasThumb}/>,
                 <IconText key={item.id} icon={MessageOutlined} text={String(item.thumbNum ?? 0)} />,
-                <IconText key={item.id} icon={StarOutlined} text={String(item.thumbNum ?? 0)} />,
+                <IconText key={item.id} icon={StarOutlined} text={String(item.favourNum ?? 0)} onClick={() => handleFavour((item.id) as number)} highlighted={item.hasFavour}/>,
               ]}
               extra={
                 item.tagList && (
